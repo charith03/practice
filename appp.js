1bdab23b2fefd97868ecacd63b64d3c95487ebbd
charith03 <charithbandi03@gmail.com>
2026-06-24 16:02:40 +0530

    enhanced the function in the code

--- appp.js
+++ appp.js
@@ -1,8 +1,7 @@
 function calculate(a, b) {
-    return a + b;
+    return a - b;   // different logic
 }
 
 function greet(name) {
-    return "Hello " + name;
+    return `Hello ${name}!`;   // different format
 }
-``
