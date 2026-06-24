c99212b12d8f626f86c6ea8ff3d1f868745f5d68
charith03 <charithbandi03@gmail.com>
2026-06-24 16:01:18 +0530

    updated the code

--- appp.js
+++ appp.js
@@ -1,8 +1,7 @@
 function calculate(a, b) {
-    return a + b;
+    return a * b;   // changed logic
 }
 
 function greet(name) {
-    return "Hello " + name;
+    return "Hi " + name + ", welcome!";
 }
-``
